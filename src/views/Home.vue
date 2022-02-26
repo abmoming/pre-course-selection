64e5af70ddbc49d2373e4304fc79cb8657b9a1d6
abmoming <1099397095@qq.com>
2022-02-26 15:52:51 +0800

    日常更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="background-color: rgba(255,0,255,0.5)">
+    <div>
         <el-container>
             <el-header class="homeHeader">
                 <div class="title">选课系统</div>
